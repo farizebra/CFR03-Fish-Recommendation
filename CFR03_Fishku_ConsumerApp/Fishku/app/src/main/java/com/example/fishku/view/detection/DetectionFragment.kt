60c20b58c6f434552c424be56214fe412b02d068
rahmatalf <rahmatalfajri1@gmail.com>
2023-12-14 23:50:51 +0700

    Page Deteksi Ikan

--- CFR03_Fishku_ConsumerApp/Fishku/app/src/main/java/com/example/fishku/view/detection/DetectionFragment.kt
+++ CFR03_Fishku_ConsumerApp/Fishku/app/src/main/java/com/example/fishku/view/detection/DetectionFragment.kt
@@ -1,32 +1,46 @@
 package com.example.fishku.view.detection
 
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.fishku.R
+import com.example.fishku.databinding.FragmentDetectionBinding
+import com.example.fishku.util.getImageUri
+import com.example.fishku.util.reduceFileImage
+import com.example.fishku.util.uriToFile
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetectionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetectionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var binding: FragmentDetectionBinding
+
+    private var currentImageUri: Uri? = null
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "Tidak ada gambar yang dipilih")
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
         }
     }
 
@@ -34,27 +48,68 @@ class DetectionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detection, container, false)
+        binding = FragmentDetectionBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetectionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetectionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnGallery.setOnClickListener { startGallery() }
+        binding.btnCamera.setOnClickListener { startCamera() }
+        binding.btnDetect.setOnClickListener {
+//            viewModel.getSession().observe(this) {user ->
+//                uploadImage(binding.descEditText.text.toString())
+//            }
+        }
     }
+
+    private fun startGallery() {
+        launcherGallery.launch("image/*")
+    }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(requireContext())
+        launcherIntentCamera.launch(currentImageUri)
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.ivImagePreview.setImageURI(it)
+        }
+    }
+
+//    private fun uploadImage(description: String) {
+//        currentImageUri?.let { uri ->
+//            val imageFile = uriToFile(uri, requireContext()).reduceFileImage()
+//            Log.d("Image File", "showImage: ${imageFile.path}")
+//            viewModel.uploadImage(imageFile, description).observe(viewLifecycleOwner) { result ->
+//                if (result != null) {
+//                    when (result) {
+//                        is ResultState.Loading -> {
+//                            showLoading(true)
+//                        }
+//                        is ResultState.Success -> {
+//                            showToast(result.data.message!!)
+//                            showLoading(false)
+//                            // You may want to handle navigation here (e.g., using Navigation Component)
+//                        }
+//                        is ResultState.Error -> {
+//                            showToast(result.error)
+//                            showLoading(false)
+//                        }
+//                    }
+//                }
+//            }
+//        } ?: showToast(getString(R.string.empty_image_warning))
+//    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+//    private fun showLoading(state: Boolean) {
+//        binding.progressBar.visibility = if (state) View.VISIBLE else View.GONE
+//    }
 }
