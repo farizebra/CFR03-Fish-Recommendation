341d5822bad8160788eaa9d76c9a73eac1fd0c5a
Faktul Diki <118914768+faktuldiki13@users.noreply.github.com>
2023-12-14 13:47:19 +0700

    menambahkan fitur
    menambahkan card item, detail

---
+++ CFR03_Fishku_ConsumerApp/Fishku/app/src/main/java/com/example/fishku/DecetionResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fishku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DecetionResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_decetion_result)
+    }
+}
