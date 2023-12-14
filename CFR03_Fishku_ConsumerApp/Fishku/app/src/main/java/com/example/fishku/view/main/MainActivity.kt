d529a75d76cfc4143366be42d2bdba0d0421be33
rahmatalf <rahmatalfajri1@gmail.com>
2023-12-14 22:18:05 +0700

    Bottom Navigation

--- CFR03_Fishku_ConsumerApp/Fishku/app/src/main/java/com/example/fishku/view/main/MainActivity.kt
+++ CFR03_Fishku_ConsumerApp/Fishku/app/src/main/java/com/example/fishku/view/main/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.fishku.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.fishku.R
+import com.example.fishku.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_detection, R.id.navigation_dictionary, R.id.navigation_account
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
